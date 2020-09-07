cff81f92d3320c9859520985c549b63be23a7728
Rion Brattig Correia <rionbr@indiana.edu>
2020-09-07 17:16:38 +0100

    update preliminary results

---
+++ experiments/cohort-analysis/01-plot-rc-age.py
@@ -0,0 +1,115 @@
+# coding=utf-8
+# Author: Rion B Correia
+# Date: April 17, 2020
+#
+# Description: Plot risk of interaction per age_group
+#
+#
+import numpy as np
+import pandas as pd
+from utils import ensurePathExists
+import statsmodels.api as sm
+import matplotlib as mpl
+mpl.rcParams['mathtext.fontset'] = 'cm'
+mpl.rcParams['mathtext.rm'] = 'serif'
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+
+    path = '../../data/'
+    dfi = pd.read_csv(path + 'indianapolis/age.csv', index_col='age_group')
+    dfb = pd.read_csv(path + 'blumenau/age.csv', index_col='age_group')
+
+    # Format Catalonia data
+    dfc = pd.read_csv(path + 'Catalonia/Age-gender.txt', sep='\t')
+    dfcp = pd.read_csv(path + 'Catalonia/Number_of_patients_per_age_range.txt', sep='\t')
+    dfc['patient'] = dfcp['Males_one_drug'] + dfcp['Females_one_drug']
+    dfc['patient-coadmin'] = dfc['Coad_fem'] + dfc['Coad_mal']
+    dfc = dfc.loc[:, ['patient-coadmin', 'patient']]
+
+    # Compute RI
+    dfi['RC^{[y1,y2]}'] = dfi['patient-coadmin'] / dfi['patient']
+    dfb['RC^{[y1,y2]}'] = dfb['patient-coadmin'] / dfb['patient']
+    dfc['RC^{[y1,y2]}'] = (dfc['patient-coadmin'] / dfc['patient']).fillna(method='ffill')
+
+    #
+    # Curve Fitting
+    #
+    y_rc_indy = dfi['RC^{[y1,y2]}'].values
+    y_rc_bnu = dfb['RC^{[y1,y2]}'].values
+    y_rc_cat = dfc['RC^{[y1,y2]}'].values
+    x = np.arange(len(y_rc_indy))
+    x_ = np.linspace(x[0], x[-1], len(x) * 10)
+
+    # RI Cubic Model
+    Xi = sm.add_constant(np.column_stack([x**3, x**2, x]))
+    rc_c_model_indy = sm.OLS(y_rc_indy, Xi)
+    rc_c_model_bnu = sm.OLS(y_rc_bnu, Xi)
+    rc_c_model_cat = sm.OLS(y_rc_cat, Xi)
+    rc_c_model_indy_result = rc_c_model_indy.fit()
+    rc_c_model_bnu_result = rc_c_model_bnu.fit()
+    rc_c_model_cat_result = rc_c_model_cat.fit()
+
+    # print(rc_c_model_result.summary())
+    Xi_ = sm.add_constant(np.column_stack([x_**3, x_**2, x_]))
+    y_rc_indy_ = np.dot(Xi_, rc_c_model_indy_result.params)
+    y_rc_bnu_ = np.dot(Xi_, rc_c_model_bnu_result.params)
+    y_rc_cat_ = np.dot(Xi_, rc_c_model_cat_result.params)
+    #
+    rc_c_model_indy_R2 = rc_c_model_indy_result.rsquared_adj
+    rc_c_model_bnu_R2 = rc_c_model_bnu_result.rsquared_adj
+    rc_c_model_cat_R2 = rc_c_model_cat_result.rsquared_adj
+
+    #
+    # Plot
+    #
+    fig, ax = plt.subplots(figsize=(4.3, 3), nrows=1, ncols=1)
+    #
+    fit_color_indy = markerfacecolor_indy = '#aec7e8'
+    markeredgecolor_indy = '#1f77b4'
+    #
+    fit_color_bnu = markerfacecolor_bnu = '#98df8a'
+    markeredgecolor_bnu = '#2ca02c'
+    #
+    fit_color_cat = markerfacecolor_cat = '#ff9896'
+    markeredgecolor_cat = '#d62728'
+
+    # color_ddi = #d62728 / #ff9896
+
+    ax.set_title(r'$RC^{[y_1,y_2]}$')
+    #
+    age_inds = np.arange(0, len(dfi))
+    age_labels = dfi.index.tolist()
+
+    # Plot
+    rc_indy, = ax.plot(age_inds, dfi['RC^{[y1,y2]}'].tolist(), marker='o', ms=6, lw=0, markerfacecolor=markerfacecolor_indy, markeredgecolor=markeredgecolor_indy, zorder=5)
+    rc_bnu, = ax.plot(age_inds, dfb['RC^{[y1,y2]}'].tolist(), marker='^', ms=6, lw=0, markerfacecolor=markerfacecolor_bnu, markeredgecolor=markeredgecolor_bnu, zorder=5)
+    rc_cat, = ax.plot(age_inds, dfc['RC^{[y1,y2]}'].tolist(), marker='p', ms=6, lw=0, markerfacecolor=markerfacecolor_cat, markeredgecolor=markeredgecolor_cat, zorder=5)
+
+    # Plot Cubic Fit
+    rc_indy_cubic, = ax.plot(x_, y_rc_indy_, color=fit_color_indy, ms=0, lw=2, zorder=3)
+    rc_bnu_cubic, = ax.plot(x_, y_rc_bnu_, color=fit_color_bnu, ms=0, lw=2, zorder=3)
+    rc_cat_cubic, = ax.plot(x_, y_rc_cat_, color=fit_color_cat, ms=0, lw=2, zorder=3)
+
+    # R^2
+    ax.text(x=0.97, y=0.23, s=r'$R^{{2}}_{{Indy}}={r2:.2f}$'.format(r2=rc_c_model_indy_R2), ha='right', va='bottom', transform=ax.transAxes)
+    ax.text(x=0.97, y=0.13, s=r'$R^{{2}}_{{Bnu}}={r2:.2f}$'.format(r2=rc_c_model_bnu_R2), ha='right', va='bottom', transform=ax.transAxes)
+    ax.text(x=0.97, y=0.03, s=r'$R^{{2}}_{{Cat}}={r2:.2f}$'.format(r2=rc_c_model_cat_R2), ha='right', va='bottom', transform=ax.transAxes)
+
+    # Legend
+    Ls = ax.legend(
+        [rc_indy, rc_bnu, rc_cat],
+        [r'$RC^{[y_1,y_2]}_{Indy}$', r'$RC^{[y_1,y_2]}_{Bnu}$', r'$RC^{[y_1,y_2]}_{Cat}$'],
+        loc='upper left', handletextpad=0.25, columnspacing=0, handlelength=1, ncol=1)
+
+    ax.set_xticks(age_inds)
+    ax.set_xticklabels(age_labels, rotation=90)
+    ax.grid()
+
+    # Save
+    plt.tight_layout()
+    wIMGfile = 'images/img-rc-age.pdf'
+    ensurePathExists(wIMGfile)
+    fig.savefig(wIMGfile)
+    plt.close()
---
+++ experiments/cohort-analysis/03-plot-ri-age-gender.py
@@ -0,0 +1,134 @@
+# coding=utf-8
+# Author: Rion B Correia
+# Date: April 17, 2020
+#
+# Description: Plot risk of interaction per age_group
+#
+#
+import numpy as np
+import pandas as pd
+from utils import ensurePathExists
+import statsmodels.api as sm
+import matplotlib as mpl
+mpl.rcParams['mathtext.fontset'] = 'cm'
+mpl.rcParams['mathtext.rm'] = 'serif'
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+
+    gender = 'Male'
+    gender_symbol = 'F' if gender == 'Female' else 'M'
+
+    path = '../../data/'
+    dfi = pd.read_csv(path + 'indianapolis/age-gender.csv', index_col='age_group')
+    dfi = dfi.loc[dfi['gender'] == gender, :]
+    #
+    dfb = pd.read_csv(path + 'blumenau/age-gender.csv', index_col='age_group')
+    dfb = dfb.loc[dfb['gender'] == gender, :]
+
+    # Format Catalonia data
+    dfc = pd.read_csv(path + 'Catalonia/Age-gender.txt', sep='\t')
+    dfcp = pd.read_csv(path + 'Catalonia/Number_of_patients_per_age_range.txt', sep='\t')
+    if gender == 'Female':
+        dfc['patient'] = dfcp['Females_one_drug']
+        dfc['patient-inter'] = dfc['Coad_DDI_fem']
+    elif gender == 'Male':
+        dfc['patient'] = dfcp['Males_one_drug'] + dfcp['Females_one_drug']
+        dfc['patient-inter'] = dfc['Coad_DDI_mal']
+    dfc = dfc.loc[:, ['patient-inter', 'patient']]
+
+    # Compute RI
+    dfi['RI^{[y1,y2],g}'] = dfi['patient-inter'] / dfi['patient']
+    dfb['RI^{[y1,y2],g}'] = dfb['patient-inter'] / dfb['patient']
+    dfc['RI^{[y1,y2],g}'] = (dfc['patient-inter'] / dfc['patient']).fillna(method='ffill')
+
+    #
+    # Curve Fitting
+    #
+    y_ri_indy = dfi['RI^{[y1,y2],g}'].values
+    y_ri_bnu = dfb['RI^{[y1,y2],g}'].values
+    y_ri_cat = dfc['RI^{[y1,y2],g}'].values
+    x = np.arange(len(y_ri_indy))
+    x_ = np.linspace(x[0], x[-1], len(x) * 10)
+
+    # RI Cubic Model
+    Xi = sm.add_constant(np.column_stack([x**3, x**2, x]))
+    ri_c_model_indy = sm.OLS(y_ri_indy, Xi)
+    ri_c_model_bnu = sm.OLS(y_ri_bnu, Xi)
+    ri_c_model_cat = sm.OLS(y_ri_cat, Xi)
+    ri_c_model_indy_result = ri_c_model_indy.fit()
+    ri_c_model_bnu_result = ri_c_model_bnu.fit()
+    ri_c_model_cat_result = ri_c_model_cat.fit()
+
+    # print(rc_c_model_result.summary())
+    Xi_ = sm.add_constant(np.column_stack([x_**3, x_**2, x_]))
+    y_ri_indy_ = np.dot(Xi_, ri_c_model_indy_result.params)
+    y_ri_bnu_ = np.dot(Xi_, ri_c_model_bnu_result.params)
+    y_ri_cat_ = np.dot(Xi_, ri_c_model_cat_result.params)
+    #
+    ri_c_model_indy_R2 = ri_c_model_indy_result.rsquared_adj
+    ri_c_model_bnu_R2 = ri_c_model_bnu_result.rsquared_adj
+    ri_c_model_cat_R2 = ri_c_model_cat_result.rsquared_adj
+
+    #
+    # Plot
+    #
+    fig, ax = plt.subplots(figsize=(4.3, 3), nrows=1, ncols=1)
+    #
+    fit_color_indy = markerfacecolor_indy = '#aec7e8'
+    markeredgecolor_indy = '#1f77b4'
+    #
+    fit_color_bnu = markerfacecolor_bnu = '#98df8a'
+    markeredgecolor_bnu = '#2ca02c'
+    #
+    fit_color_cat = markerfacecolor_cat = '#ff9896'
+    markeredgecolor_cat = '#d62728'
+
+    # color_ddi = #d62728 / #ff9896
+
+    ax.set_title(r'$RI^{{[y_1,y_2],{g:s}}}$'.format(g=gender_symbol))
+    #
+    age_inds = np.arange(0, len(dfi))
+    age_labels = dfi.index.tolist()
+
+    # Plot
+    ri_indy, = ax.plot(age_inds, dfi['RI^{[y1,y2],g}'].tolist(), marker='o', ms=6, lw=0, markerfacecolor=markerfacecolor_indy, markeredgecolor=markeredgecolor_indy, zorder=5)
+    ri_bnu, = ax.plot(age_inds, dfb['RI^{[y1,y2],g}'].tolist(), marker='^', ms=6, lw=0, markerfacecolor=markerfacecolor_bnu, markeredgecolor=markeredgecolor_bnu, zorder=5)
+    ri_cat, = ax.plot(age_inds, dfc['RI^{[y1,y2],g}'].tolist(), marker='p', ms=6, lw=0, markerfacecolor=markerfacecolor_cat, markeredgecolor=markeredgecolor_cat, zorder=5)
+
+    # Plot Cubic Fit
+    ri_indy_cubic, = ax.plot(x_, y_ri_indy_, color=fit_color_indy, ms=0, lw=2, zorder=3)
+    ri_bnu_cubic, = ax.plot(x_, y_ri_bnu_, color=fit_color_bnu, ms=0, lw=2, zorder=3)
+    ri_cat_cubic, = ax.plot(x_, y_ri_cat_, color=fit_color_cat, ms=0, lw=2, zorder=3)
+
+    # R^2
+    ax.text(x=0.97, y=0.23, s=r'$R^{{2}}_{{Indy}}={r2:.2f}$'.format(r2=ri_c_model_indy_R2), ha='right', va='bottom', transform=ax.transAxes)
+    ax.text(x=0.97, y=0.13, s=r'$R^{{2}}_{{Bnu}}={r2:.2f}$'.format(r2=ri_c_model_bnu_R2), ha='right', va='bottom', transform=ax.transAxes)
+    ax.text(x=0.97, y=0.03, s=r'$R^{{2}}_{{Cat}}={r2:.2f}$'.format(r2=ri_c_model_cat_R2), ha='right', va='bottom', transform=ax.transAxes)
+
+    # Legend
+    Ls = ax.legend(
+        [
+            ri_indy,
+            ri_bnu,
+            ri_cat
+        ],
+        [
+            r'$RI^{{[y_1,y_2],{g:s}}}_{{Indy}}$'.format(g=gender_symbol),
+            r'$RI^{{[y_1,y_2],{g:s}}}_{{Bnu}}$'.format(g=gender_symbol),
+            r'$RI^{{[y_1,y_2],{g:s}}}_{{Cat}}$'.format(g=gender_symbol)
+        ],
+        loc='upper left', handletextpad=0.25, columnspacing=0, handlelength=1, ncol=1)
+
+    ax.set_xticks(age_inds)
+    ax.set_xticklabels(age_labels, rotation=90)
+    ax.grid()
+
+    # Save
+    plt.tight_layout()
+    gender_str = gender.lower()
+    wIMGfile = 'images/img-ri-age-{gender:s}.pdf'.format(gender=gender_str)
+    ensurePathExists(wIMGfile)
+    fig.savefig(wIMGfile)
+    plt.close()
